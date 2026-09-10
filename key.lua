00fc1bdc0c483a49b2bcd018712a55433b6eada7
vuh60670-sudo <vuh60670@gmail.com>
2026-09-10 16:21:30 +0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -5,7 +5,7 @@ local Config = {
     PlatoSecret     = "74dacac1-d04f-41b1-b84c-f6e76ad40cfa", -- Your PlatoBoost Secret Key
 
     -- [2] Anti-Bypass / Global Secret Variable
-    Secret          = "Code", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
+    Secret          = "2000", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
     
     -- [3] Scripts & Links
     MainScriptURL   = "https://raw.githubusercontent.com/vuh60670-sudo/tbs/refs/heads/main/script.lua", -- The raw URL of your main script
